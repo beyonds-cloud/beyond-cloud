731b25046a4cb568da90c606badfa448c3edee6b
AryaanSheth <avsheth03@gmail.com>
2025-04-25 13:55:41 -0400

    fix: pnpm build errors/warnings

--- src/components/ui/carousel-wrapper.tsx
+++ src/components/ui/carousel-wrapper.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import Image from "next/image";
 import React from "react";
+import type { ResponsiveType } from "react-multi-carousel";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 
@@ -11,19 +13,21 @@ const CarouselWrapper = ({
 }: {
   images: string[];
   locations: string[];
-  responsive: any;
+  responsive: ResponsiveType;
 }) => {
   return (
     <Carousel responsive={responsive} infinite autoPlay autoPlaySpeed={3000}>
       {images.map((image, index) => (
         <div key={index} className="px-2">
-          <img
+          <Image
             src={image}
             alt={`carousel-${index}`}
             className="rounded-xl shadow-lg w-full h-64 object-cover"
+            width={500}
+            height={256}
           />
           <p className="mt-2 text-center text-base text-[#A0AEC0]">
-            {locations[index] || "Unknown Location"}
+            {locations[index] ?? "Unknown Location"}
           </p>
         </div>
       ))}
